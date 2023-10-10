6eefbaee80b24ecbe29127db7a71391b6f6f4ee7
Waldemar Muhl <waldemarmuhl@gmail.com>
2023-10-10 16:14:15 +0100

    overcomplicated things a bit

--- EasyTests/Controllers/WeatherForecastController.cs
+++ EasyTests/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using EasyTests.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyTests.Controllers;
@@ -6,27 +7,18 @@ namespace EasyTests.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IWeatherForecastService _forecastService;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService forecastService)
     {
         _logger = logger;
+        _forecastService = forecastService;
     }
-
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return _forecastService.GetWeatherForecast();
     }
+
 }
